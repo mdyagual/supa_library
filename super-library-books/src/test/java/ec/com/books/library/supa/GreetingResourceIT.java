75df0a4b228ec641f59d597c5dec8702f1103654
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2023-05-22 23:44:07 -0500

    Initial commit that includes configuration and dependecies to use

---
+++ super-library-books/src/test/java/ec/com/books/library/supa/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package ec.com.books.library.supa;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
